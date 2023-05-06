6d9a65e97d3f42ac0e99a8aef91e77aeab9ee2d4
Forest Belton <65484+forestbelton@users.noreply.github.com>
2023-05-06 04:47:22 -0400

    Use subprocess and preserve env better

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -1,3 +1,19 @@
+@echo off
+set oldlib=%lib%
+set oldinclude=%include%
+set oldpath=%path%
+
+if /I "%1" EQU "" goto fork
+if /i "%1" EQU "run" goto run
+
+echo "unknown argument: '%1'"
+goto :end
+
+:fork
+(call %0 run)
+goto :end
+
+:run
 cd c:\decompile\SAMEBOY
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 set lib=%lib%;C:\decompile\SDL2\lib\x86
@@ -5,3 +21,8 @@ set include=%include%;C:\decompile\SDL2\include
 set path=%path%;C:\decompile\rgbds;C:\msys64\usr\bin
 make
 pause
+
+:end
+set lib=%oldlib%
+set include=%oldinclude%
+set path=%oldpath%
