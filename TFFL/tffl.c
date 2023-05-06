898585a192a245bf806247b21407b2778aa67563
Forest Belton <65484+forestbelton@users.noreply.github.com>
2023-05-06 06:23:36 -0400

    Improve logging from hook DLL

--- TFFL/tffl.c
+++ TFFL/tffl.c
@@ -1,5 +1,7 @@
 #include "gb.h"
+#include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
 #include <Windows.h>
 
 FILE *LOG;
@@ -15,13 +17,32 @@ struct GB_hooklist_s HOOKLIST = {
     .entry = &HOOKS[0]
 };
 
+const char *now() {
+    static struct tm tm;
+    static time_t now;
+    static char buf[sizeof "2011-10-08T07:07:09Z"];
+    time(&now);
+    gmtime_s(&tm, &now);
+    strftime(&buf[0], sizeof buf, "%FT%TZ", &tm);
+    return &buf[0];
+}
+
+void tlogf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    fprintf(LOG, "%s - ", now());
+    vfprintf(LOG, fmt, args);
+    fprintf(LOG, "\n");
+    va_end(args);
+}
+
 __declspec(dllexport) struct GB_hooklist_s *GetHookList() {
     fopen_s(&LOG, "tffl.log", "a+");
-    fprintf(LOG, "[HOOK-DLL] Logger initialized\n");
+    tlogf("Logger initialized");
     return &HOOKLIST;
 }
 
 unsigned hook_entry(GB_gameboy_t *gb) {
-    fprintf(LOG, "[HOOK-DLL] Entry point hooked\n");
+    tlogf("Entry point hooked");
     return 0;
 }
